7e307a28ae59eb38df278e792338286a2742ef23
saumya <saumya@pivotaldesign.biz>
2016-08-19 17:04:00 +0530

    stored in NSUserDefaults

--- LocalData/ViewController.swift
+++ LocalData/ViewController.swift
@@ -8,7 +8,14 @@
 
 import UIKit
 
+struct myDefaultsKeys {
+    static let keyOne = "firstStringKey"
+    static let keyTwo = "secondStringKey"
+}
+
 class ViewController: UIViewController {
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +28,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+
+extension ViewController {
+    @IBAction func onSaveLocal(sender:AnyObject){
+        // NSUserDefaults
+        
+        print("onSaveLocal:NSUserDefaults:  ==========")
+        
+        // ref : http://stackoverflow.com/questions/28628225/how-do-you-save-local-storage-data-in-a-swift-application
+        
+        defaults.setValue("String 1", forKey: myDefaultsKeys.keyOne)
+        defaults.setValue("String 2", forKey: myDefaultsKeys.keyTwo)
+        
+        defaults.synchronize()
+        
+        print("onSaveLocal:NSUserDefaults: /==========")
+    }
+
+    @IBAction func onGetLocal(sender:AnyObject){
+        //
+        print("onGetLocal:NSUserDefaults:  ======== ")
+        
+        if let stringOne = defaults.stringForKey(myDefaultsKeys.keyOne) {
+            print("keyOne:",stringOne) // Some String Value
+        }
+        
+        if let stringTwo = defaults.stringForKey(myDefaultsKeys.keyTwo) {
+            print("keyTwo:",stringTwo) // Another String Value
+        }
+        
+        print("onGetLocal:NSUserDefaults: /======== ")
+    }
+}
+
+
+//
